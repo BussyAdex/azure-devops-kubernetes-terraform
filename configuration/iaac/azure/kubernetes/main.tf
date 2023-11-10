a8e292430636e41fe2148c1e156d30d928b18521
Bussy Adex <68420317+BussyAdex@users.noreply.github.com>
2023-11-10 01:04:07 +0000

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -25,8 +25,8 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-    # vm_size         = "standard_b2ms" CHANGE IF AN ERROR ARISES
-    vm_size         = "Standard_B1s"       
+    vm_size         = "Standard_DS1_v2" 
+    # vm_size         = "Standard_B1s"       
   }
 
   service_principal {
