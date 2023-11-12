32e00e7f77c705e37e64905f894e489869f6b2d8
Bussyadex <Bussyadex42@gmail.com>
2023-11-12 23:34:08 +0000

    go

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,76 +1,66 @@
-# aws --version
-# aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
-# Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
-# terraform-backend-state-in28minutes-123
-# AKIA4AHVNOD7OOO6T4KI
-
-
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.57.0"  
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10.0" 
+    }
+  }
   backend "s3" {
-    bucket = "mybucket" # Will be overridden from build
-    key    = "path/to/my/key" # Will be overridden from build
+    bucket = "terraform-backend-state-bussyadex42-123"
+    key    = "kubernetes-dev.tfstate" 
     region = "us-east-1"
   }
 }
 
-resource "aws_default_vpc" "default" {
-
+provider "aws" {
+  region = "us-east-1"
 }
+
+resource "aws_default_vpc" "default" {}
 
 data "aws_subnet_ids" "subnets" {
   vpc_id = aws_default_vpc.default.id
 }
 
-
 provider "kubernetes" {
-  //>>Uncomment this section once EKS is created - Start
-  host                   = data.aws_eks_cluster.cluster.endpoint #module.in28minutes-cluster.cluster_endpoint
+  host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  //>>Uncomment this section once EKS is created - End
+  load_config_file       = false
 }
 
-module "in28minutes-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "in28minutes-cluster"
-  cluster_version = "1.23"
-  subnet_ids         = ["subnet-0ba16627", "subnet-02db6b4a", "subnet-e08479ba"] #CHANGE # Donot choose subnet from us-east-1e
+module "bussyadex42-cluster" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.0.3"  
+
+  cluster_name    = "bussyadex42-cluster"
+  cluster_version = "1.21"  
   vpc_id          = aws_default_vpc.default.id
+  subnet_ids      = data.aws_subnet_ids.subnets.ids  
 
-  //Newly added entry to allow connection to the api server
-  //Without this change error in step 163 in course will not go away
-  cluster_endpoint_public_access  = true
-
-# EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    instance_types = ["t2.small", "t2.medium"]
-  }
-
-  eks_managed_node_groups = {
-    blue = {}
-    green = {
-      min_size     = 1
-      max_size     = 10
-      desired_size = 1
-
-      instance_types = ["t2.medium"]
+  node_groups = {
+    example = {
+      instance_type     = "t2.micro"
+      max_capacity      = 5
+      desired_capacity  = 3
+      min_capacity      = 1
+      // Add other required node group configuration here
     }
   }
 }
 
-//>>Uncomment this section once EKS is created - Start
- data "aws_eks_cluster" "cluster" {
-   name = "in28minutes-cluster" #module.in28minutes-cluster.cluster_name
- }
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = "in28minutes-cluster" #module.in28minutes-cluster.cluster_name
+data "aws_eks_cluster" "cluster" {
+  name = module.bussyadex42-cluster.cluster_id
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.bussyadex42-cluster.cluster_id
+}
 
-# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
-# ServiceAccount needs permissions to create deployments 
-# and services in default namespace
 resource "kubernetes_cluster_role_binding" "example" {
   metadata {
     name = "fabric8-rbac"
@@ -85,10 +75,4 @@ resource "kubernetes_cluster_role_binding" "example" {
     name      = "default"
     namespace = "default"
   }
-}
-//>>Uncomment this section once EKS is created - End
-
-# Needed to set the default region
-provider "aws" {
-  region  = "us-east-1"
 }
