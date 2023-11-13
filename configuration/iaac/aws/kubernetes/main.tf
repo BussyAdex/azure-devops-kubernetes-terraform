1a1b1bf218d5be234b6103c4b261dec887488986
Bussyadex <Bussyadex42@gmail.com>
2023-11-13 23:12:39 +0000

    try

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,8 +1,16 @@
-module "eks" {
+terraform {
+  backend "s3" {
+    bucket = "mybucket" # Will be overridden from build
+    key    = "path/to/my/key" # Will be overridden from build
+    region = "us-east-1"
+  }
+}
+
+module "bussyadex42-cluster" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = "my-cluster"
+  cluster_name    = "mybussy-cluster"
   cluster_version = "1.27"
 
   cluster_endpoint_public_access  = true
